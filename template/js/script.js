5f9e62786c92312996caa2d289f4f469b7693f23
sychevskiy <sychevskiy.is@unilab.su>
2019-11-14 17:49:24 +1000

    Add table for cocktail on first section

--- template/js/script.js
+++ template/js/script.js
@@ -1,4 +1,24 @@
 $( document ).ready(function() {
+    // Массив коктелей под первый блок
+    let dict = {
+        "icon-cocktail1": {
+            "title": "Маргарита",
+            "subTitle": "алк. коктель",
+            "desc": "Описание коктеля"
+        },
+        "icon-cocktail2": {
+            "title": "Маргарита",
+            "subTitle": "алк. коктель",
+            "desc": "Описание коктеля"
+        },
+        "icon-cocktail3": {
+            "title": "Маргарита",
+            "subTitle": "алк. коктель",
+            "desc": "Описание коктеля"
+        }
+    };
+    
+    // При клике на 1 секцию на 1 блоке
     $( document ).on("click", ".main__sh-item_outside", function() {
         $( this ).addClass("main__sh-item_outside-active");
         
